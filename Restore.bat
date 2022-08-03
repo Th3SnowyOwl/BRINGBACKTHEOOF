d1e2af3eeacfe6fc15fa6eae5814712528e3fdd0
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-08-02 23:49:29 -0400

    Update Restore.bat
    x86 fix

--- Restore.bat
+++ Restore.bat
@@ -2,7 +2,18 @@ cd %appdata%
 cd ..
 cd local/Roblox/Versions/
 for /f "tokens=*" %%a in ('dir RobloxPlayerLauncher.exe /b /s') do set playerdir=%%a
+if exist %playerdir% goto cont1
+:cont1
 cd %playerdir:RobloxPlayerLauncher.exe=%
 cd content/sounds/
 del ouch.ogg
-curl -s -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
+curl -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
+cd C:\Program Files (x86)\Roblox\Versions\
+for /f "tokens=*" %%a in ('dir RobloxPlayerLauncher.exe /b /s') do set playerdir=%%a
+if exist %playerdir% (goto cont2) else (exit)
+:cont2
+cd %playerdir:RobloxPlayerLauncher.exe=%
+cd content/sounds/
+del ouch.ogg
+curl -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
+pause

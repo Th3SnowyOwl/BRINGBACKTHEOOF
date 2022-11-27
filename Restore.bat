c1e0c99b6cbfbe1599df58d5f2778ed4e65836d8
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-11-26 19:50:28 -0500

    bugfix
    removed bugged lines, dupe temp file removed, causing 403 auth error

--- Restore.bat
+++ Restore.bat
@@ -12,10 +12,6 @@ del ouch.ogg
 curl -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
 cd C:\Program Files (x86)\Roblox\Versions\
 for /f "tokens=*" %%a in ('dir RobloxPlayerLauncher.exe /b /s') do set playerdir=%%a
-echo %playerdir% > temp
-set /p temp=<temp
-if not "%temp%" == "%playerdir%" (goto dhur) else (exit)
-:dhur
 if exist %playerdir% (goto cont2) else (exit)
 :cont2
 cd %playerdir:RobloxPlayerLauncher.exe=%

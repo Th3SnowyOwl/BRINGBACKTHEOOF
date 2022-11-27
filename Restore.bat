5a19b34c655a85581b88e1d0742cca458f62a2f3
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-11-26 19:16:08 -0500

    Add files via upload

--- Restore.bat
+++ Restore.bat
@@ -2,18 +2,25 @@ cd %appdata%
 cd ..
 cd local/Roblox/Versions/
 for /f "tokens=*" %%a in ('dir RobloxPlayerLauncher.exe /b /s') do set playerdir=%%a
-if exist %playerdir% goto cont1
-:cont1
+echo %playerdir% > temp
+set /p temp=<temp
+if not "%temp%" == "%playerdir%" (goto cont) else (exit)
+:cont
 cd %playerdir:RobloxPlayerLauncher.exe=%
 cd content/sounds/
 del ouch.ogg
 curl -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
 cd C:\Program Files (x86)\Roblox\Versions\
 for /f "tokens=*" %%a in ('dir RobloxPlayerLauncher.exe /b /s') do set playerdir=%%a
+echo %playerdir% > temp
+set /p temp=<temp
+if not "%temp%" == "%playerdir%" (goto dhur) else (exit)
+:dhur
 if exist %playerdir% (goto cont2) else (exit)
 :cont2
 cd %playerdir:RobloxPlayerLauncher.exe=%
 cd content/sounds/
 del ouch.ogg
 curl -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
-pause
+
+

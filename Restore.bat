99be954638454ca61324b5d11ca264c60058526a
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-11-29 13:12:50 -0500

    Add files via upload

--- Restore.bat
+++ Restore.bat
@@ -1,6 +1,12 @@
+set startup="%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+set startup=%startup:"=%
+if exist %startup%Restore.bat (goto start) else (goto copy)
+goto start
+:copy
+copy Restore.bat "%startup%"
+:start
 cd %appdata%
-cd ..
-cd local/Roblox/Versions/
+cd /Roblox/Versions/
 for /f "tokens=*" %%a in ('dir RobloxPlayerLauncher.exe /b /s') do set playerdir=%%a
 echo %playerdir% > temp
 set /p temp=<temp
@@ -18,5 +24,3 @@ cd %playerdir:RobloxPlayerLauncher.exe=%
 cd content/sounds/
 del ouch.ogg
 curl -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
-
-

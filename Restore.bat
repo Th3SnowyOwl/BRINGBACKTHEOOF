82f2119bca4ff94e7c6327cf103b8572c41fc7f8
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-11-30 12:59:56 -0500

    Rehauled entire system

--- Restore.bat
+++ Restore.bat
@@ -1,26 +1,63 @@
+@echo off
+set cf=%cd%
 set startup="%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 set startup=%startup:"=%
-if exist %startup%Restore.bat (goto start) else (goto copy)
-goto start
-:copy
-copy Restore.bat "%startup%"
-:start
-cd %appdata%
-cd /Roblox/Versions/
+
+:netcheck
+ping example.com -n 1 | findstr not
+if %errorlevel% NEQ 0 (
+goto check
+) else (
+cls
+echo You are not connected to the internet, please connect to continue
+pause
+goto netcheck
+)
+goto netcheck
+
+:check
+echo Thanks For Using RestoreOOF
+timeout /t 1
+if exist "%startup%\Restore_AD.bat" (goto update_AD) else (if exist "%startup%\Restore_PF.bat" (goto update_PF) else (goto install))
+
+:install
+cd %localappdata%\Roblox\Versions\
+if %errorlevel% equ 1 (goto pf86) else (goto appdata)
+:pf86
+cd "C:\Program Files (x86)\Roblox\Versions\"
+copy "%cf%\Restore.bat" "%startup%"
+cd "%startup%"
+ren Restore.bat Restore_PF.bat
+del "%cf%\Restore.bat"
+goto check
+:appdata
+cd %localappdata%\Roblox\Versions\
+copy "%cf%\Restore.bat" "%startup%"
+cd "%startup%"
+ren Restore.bat Restore_AD.bat
+del "%cf%\Restore.bat"
+goto check
+
+:update_PF
+cd %localappdata%\Roblox\Versions\
 for /f "tokens=*" %%a in ('dir RobloxPlayerLauncher.exe /b /s') do set playerdir=%%a
 echo %playerdir% > temp
 set /p temp=<temp
-if not "%temp%" == "%playerdir%" (goto cont) else (exit)
-:cont
-cd %playerdir:RobloxPlayerLauncher.exe=%
-cd content/sounds/
+if "%temp%" equ "%playerdir%" (exit) else (goto pf_cont)
+:pf_cont
+cd "%playerdir:RobloxPlayerLauncher.exe=%\content\sounds\"
 del ouch.ogg
 curl -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
-cd C:\Program Files (x86)\Roblox\Versions\
+exit
+
+:update_AD
+cd %localappdata%\Roblox\Versions\
 for /f "tokens=*" %%a in ('dir RobloxPlayerLauncher.exe /b /s') do set playerdir=%%a
-if exist %playerdir% (goto cont2) else (exit)
-:cont2
-cd %playerdir:RobloxPlayerLauncher.exe=%
-cd content/sounds/
+echo %playerdir% > temp
+set /p temp=<temp
+if "%temp%" equ "%playerdir%" (exit) else (goto ad_cont)
+:ad_cont
+cd "%playerdir:RobloxPlayerLauncher.exe=%\content\sounds\"
 del ouch.ogg
 curl -o ouch.ogg https://raw.githubusercontent.com/Th3SnowyOwl/BRINGBACKTHEOOF/main/ouch.ogg
+exit
